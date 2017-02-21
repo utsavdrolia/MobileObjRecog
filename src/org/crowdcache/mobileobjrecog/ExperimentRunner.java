214932b3804065b30617d487d42047132810af4b
Utsav Drolia <utsavdrolia@gmail.com>
2017-02-21 17:33:05 -0500

    Android Activity shim layer over object recognition functions for experiments with devices. Type of experiment is Controlled by intent delivered to the app.

--- src/org/crowdcache/mobileobjrecog/ExperimentRunner.java
+++ src/org/crowdcache/mobileobjrecog/ExperimentRunner.java
@@ -1,15 +1,11 @@
 package org.crowdcache.mobileobjrecog;
 
-import edu.cmu.edgecache.objrec.opencv.Util;
 import edu.cmu.edgecache.objrec.rpc.ObjRecClient;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+
 
 import java.io.*;
-import java.util.Collection;
-import java.util.Map;
-import java.util.concurrent.ConcurrentLinkedQueue;
+
+import static edu.cmu.edgecache.objrec.opencv.Util.evaluateAsync;
 
 /**
  * Runs the trace based experiments
@@ -17,11 +13,6 @@ import java.util.concurrent.ConcurrentLinkedQueue;
  */
 public class ExperimentRunner extends Thread
 {
-    private static final String REQUEST = "request";
-    private static final String STARTTIME = "start";
-    private static final String ENDTIME = "end";
-    private static final String RESPONSE = "response";
-    private static final String LATENCIES = "latencies";
     private final String FINISH;
     private final String resultspath;
     private ObjRecClient mObjRecClient;
@@ -54,94 +45,20 @@ public class ExperimentRunner extends Thread
     {
         try
         {
+            // Delete any old finish file
             File finish = new File(FINISH);
             finish.delete();
 
-
-            ConcurrentLinkedQueue<Util.EvaluateCallback> evaluateCallbacks = new ConcurrentLinkedQueue<>();
-            BufferedReader trace = new BufferedReader(new FileReader(tracepath));
-            BufferedWriter resultsfile = new BufferedWriter(new FileWriter(resultspath));
-
-            Integer count = 0;
-            String line = trace.readLine();
-            Long procstart = System.currentTimeMillis();
-            do
-            {
-                // Parse
-                String[] chunks = line.split(",");
-                String img = chunks[0];
-                String imgpath = chunks[1];
-                Long req_time = Long.valueOf(chunks[2]);
-
-                // Create callback
-                Util.EvaluateCallback cb = new Util.EvaluateCallback(System.currentTimeMillis(), img);
-
-                // Wait till assigned time
-                while((System.currentTimeMillis() - procstart) < req_time)
-                    sleep(100l);
-                mObjRecClient.recognize(imgpath, cb);
-                evaluateCallbacks.add(cb);
-
-//            resultsfile.write(img.split("_")[0] + "," + resultMap.get(img).annotation + "," + (1 - (resultMap.get(img).time.size() - 1)) + "," + "\n");
-                line = trace.readLine();
-                count++;
-            } while ((line != null));
-//        System.out.println("Results:\n" + resultMap.toString());
-
-            Long procend = System.currentTimeMillis() - procstart;
-            System.out.println("Time:" + procend + " Count:" + count);
-
-            // Write results to file
-            writeResultFile(evaluateCallbacks, resultsfile);
-            resultsfile.flush();
-            resultsfile.close();
+            evaluateAsync(mObjRecClient, tracepath, resultspath);
 
             // Create finish file
             BufferedWriter finishfile = new BufferedWriter(new FileWriter(FINISH));
             finishfile.write("1");
             finishfile.close();
         }
-        catch (IOException | InterruptedException | JSONException e)
+        catch (IOException | InterruptedException e)
         {
             e.printStackTrace();
         }
     }
-
-    /**
-     * Writes JSON file. Format
-     * [{REQUEST:"",
-     *   START:ms,
-     *   END:ms,
-     *   RESPONSE:"",
-     *   LATENCIES:{DEVICE:{QUEUE:ms, COMPUTE:ms, NEXT:ms}, ... }},
-     *   {REQUEST:"", ...}
-     *  ]
-     * @param evaluateCallbacks
-     * @param resultsfile
-     * @throws IOException
-     */
-    private void writeResultFile(Collection<Util.EvaluateCallback> evaluateCallbacks, BufferedWriter resultsfile) throws IOException, JSONException
-    {
-        JSONArray output_array = new JSONArray();
-        for(Util.EvaluateCallback callback: evaluateCallbacks)
-        {
-            // Ensure callback is processed
-            while(!callback.isDone());
-            Util.Result result = callback.getResult();
-            JSONObject json = new JSONObject();
-
-            Map<String, Map<String, Integer>> map = result.getLatencies();
-            json.put(REQUEST, callback.getQuery());
-            json.put(STARTTIME, callback.getStartime());
-            json.put(ENDTIME, callback.getEndtime());
-            json.put(RESPONSE, result.getAnnotation());
-            json.put(LATENCIES, map);
-
-            output_array.put(json);
-        }
-
-        resultsfile.write(output_array.toString());
-    }
-
-
 }
---
+++ src/org/crowdcache/mobileobjrecog/TraceRunnerActivity.java
@@ -0,0 +1,83 @@
+package org.crowdcache.mobileobjrecog;
+
+import android.content.Intent;
+import edu.cmu.edgecache.objrec.opencv.Util;
+import edu.cmu.edgecache.objrec.rpc.Names;
+import edu.cmu.edgecache.objrec.rpc.ObjRecClient;
+
+import java.io.IOException;
+
+/**
+ * This application for mobile recognition uses a local recognition cache
+ */
+public class TraceRunnerActivity extends ExperimentActivity
+{
+    private static final String DB = "DBPATH";
+    private static final String QUERY = "QUERYPATH";
+    private static final String FEATURE = "FEATURETYPE";
+    private static final String FEATURE_PARS = "FEATUREPARSPATH";
+    private static final String MATCHER = "MATCHERTYPE";
+    private static final String MATCHER_PARS = "MATCHERPARSPATH";
+    private static final String FINISHPATH = "FINISHPATH";
+    private static final String RESULTPATH = "RESULTPATH";
+    private static final String SERVER_ADD = "SERVER_ADD";
+    private static final String F_K_PARS = "F_K";
+    private static final String RECALL_PARS = "RECALL_K";
+    private static final String ALL_OBJECTS = "ALL_OBJECTS";
+
+    private static final String EXPERIMENT_TYPE = "EXP_TYPE";
+    private static final String DEVICE_ONLY = "DEVICE_ONLY";
+    private static final String SERVER_BASED = "SERVER_BASED";
+    private static final String DEVICE_CACHE = "DEVICE_CACHE";
+
+    private ExperimentRunner mRunner;
+
+    @Override
+    public void experimentIntentReceived(Intent intent)
+    {
+        ObjRecClient objRecClient = null;
+        try
+        {
+            switch (intent.getStringExtra(EXPERIMENT_TYPE))
+            {
+                case DEVICE_ONLY:
+                    break;
+                case SERVER_BASED:
+                    objRecClient = new ObjRecClient(intent.getStringExtra(SERVER_ADD));
+                    break;
+                case DEVICE_CACHE:
+                    objRecClient = Util.createOptCacheObjRecClient(intent.getIntExtra(FEATURE,
+                                                                                               Util.ORB),
+                                                                            intent.getStringExtra(
+                                                                                    FEATURE_PARS),
+                                                                            intent.getIntExtra(MATCHER,
+                                                                                               Util.BIN_NN),
+                                                                            intent.getStringExtra(
+                                                                                    MATCHER_PARS),
+                                                                            3,
+                                                                            0.5,
+                                                                            intent.getStringExtra(
+                                                                                    SERVER_ADD),
+                                                                            Names.Edge,
+                                                                            intent.getStringExtra(F_K_PARS),
+                                                                            intent.getStringExtra(
+                                                                                    RECALL_PARS),
+                                                                            intent.getStringExtra(
+                                                                                    ALL_OBJECTS));
+                    break;
+                default:
+                    // Server based
+                    objRecClient = new ObjRecClient(intent.getStringExtra(SERVER_ADD));
+            }
+            mRunner = new ExperimentRunner(
+                    objRecClient,
+                    intent.getStringExtra(FINISHPATH),
+                    intent.getStringExtra(RESULTPATH), intent.getStringExtra(QUERY)
+            );
+            mRunner.start();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
